391a4c5538d4f48a6bd4bf8f873591e010b6dcee
Pinak08 <31927067+Pinak08@users.noreply.github.com>
2020-08-22 10:25:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import datetime
+import ipaddress
+import argumentparser
+from Target.target import Target
+
+
+def initialrun():
+    """This function checks if the program is run for the first time"""
+    logfilepath = os.path.join(os.getcwd(),"access.log")
+    if not os.path.isfile(logfilepath):
+        logfile = open(logfilepath,"x")
+        logfile.write("The application is created and the log file made on {}".format(datetime.datetime.now()))
+
+def main():
+    parser = argumentparser.parsing()
+    args = parser.parse_args()
+    if args is not None:
+        try:
+            ipaddr = ipaddress.ip_address(args.target)
+            ipaddrstr = args.target
+            prtscan = Target(target=ipaddrstr, portrange=args.portrange, osscan=args.osscan, synscan=args.synscan, verbose=args.verbose)
+        except ValueError:
+            sys.exit("Invalid IP Address entered. \nProgram now exiting")
+if __name__ == '__main__':
+    main()
